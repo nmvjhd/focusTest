a44385fadbbd1d5b35c1c8931fcaee85ba69d023
nmvjhd <269954102@qq.com>
2018-05-19 00:38:31 +0800

    update

--- core/listFocus.js
+++ core/listFocus.js
@@ -3,81 +3,82 @@
  */
 
 function listFocus(options) {
-    // 横向
-    const ORITATION = 'vert';   // land | vert
-    let ids = {}; // { 1: elem1, 2: elem2, 3: elem3, 4: elem4 }
-    let curId = null;
+  // 横向
+  const ORITATION = 'vert';   // land | vert
+  let ids = {}; // { 1: elem1, 2: elem2, 3: elem3, 4: elem4 }
+  let curId = null;
+  let outId = null;
 
-    init(options);
-    function init(options) {
+  init(options);
 
-        const elems = options.elem.querySelectorAll('[focus-id]');
-        ids = Array.from(elems).reduce((prev, cur) => {
-            prev[cur.getAttribute('focus-id')] = cur;
-            return prev;
-        }, {});
+  function init(options) {
 
-        focus(curId);
-    }
+    const elems = options.elem.querySelectorAll('[focus-id]');
+    ids = Array.from(elems).reduce((prev, cur) => {
+      prev[cur.getAttribute('focus-id')] = cur;
+      return prev;
+    }, {});
 
-    // focus算法
-    function newId(curId, direct, oritation) {
-        if(oritation === 'vert') {
-            if(direct === 'left' || direct === 'right') {
-                return null;
-            } else if(direct === 'up'){
-                if(ids[curId - 1]) {
-                    return curId - 1;
-                } else {
-                    return null;
-                }
-            } else if(direct === 'down') {
-                if(ids[curId + 1]) {
-                    return curId + 1;
-                } else {
-                    return null;
-                }
-            }
+    focus(curId);
+  }
+
+  // focus算法
+  function newId(curId, direct, oritation) {
+    if (oritation === 'vert') {
+      if (direct === 'left' || direct === 'right') {
+        return null;
+      } else if (direct === 'up') {
+        if (ids[curId - 1]) {
+          return curId - 1;
+        } else {
+          return null;
         }
-    }
-
-    function focus(id) {
-        const oldId = curId;
-        if(oldId && oldId !== id) {
-            ids[oldId].classList.remove('focus');
+      } else if (direct === 'down') {
+        if (ids[curId + 1]) {
+          return curId + 1;
+        } else {
+          return null;
         }
-        if(id) {
-          ids[id].classList.add('focus');
-        }
-        curId = id;
+      }
     }
+  }
 
-    function update(direct) {
-        const nId = newId(curId, direct, ORITATION);
-        if (nId) {
-            focus(nId);
-        }
-        else {
-            scopeOut();
-        }
-        curId = nId;
-        return curId;
+  function focus(id) {
+    const oldId = curId;
+    if (oldId && oldId !== id) {
+      ids[oldId].classList.remove('focus');
+      curId = null;
     }
+    if (id) {
+      ids[id].classList.add('focus');
+      curId = id;
+    }
+  }
 
-    function scopeIn() {
-      focus(1);
+  function move(direct) {
+    const nId = newId(curId, direct, ORITATION);
+    if (nId) {
+      focus(nId);
+      return true;
     }
+    return false;
+  }
 
-    function scopeOut() {
-      focus(null);
-    }
+  function scopeIn() {
+    console.log('scope in');
+    const id = outId ? outId : 1;
+    focus(id);
+  }
 
-    return {
-        up: () => update('up'),
-        down: () => update('down'),
-        left: () => update('left'),
-        right: () => update('right'),
-        scopeIn,
-        scopeOut,
-    }
+  function scopeOut() {
+    console.log('scope out');
+    outId = curId;
+    focus(null);
+  }
+
+  return {
+    move,
+    scopeIn,
+    scopeOut,
+  }
 }
--- core/scopeFocus.js
+++ core/scopeFocus.js
@@ -4,94 +4,95 @@
 
 function createScope(option) {
 
-    return {
-        id: option.id,
-        scope: option.scope,
-    }
+  return {
+    id: option.id,
+    list: option.list,
+  }
 }
 
 function scopeFocus(options) {
-    // 横向
-    const ORITATION = 'vert';   // land | vert
-    let ids = {}; // { 1: scope1, 2: scope2, 3: scope3, 4: scope4 }
-    const scopeIn = () => console.log('scopeIn');
-    const scopeOut = () => console.log('scopeOut');
-    let curId = null;
+  // 横向
+  const ORITATION = 'vert';   // land | vert
+  let ids = {}; // { 1: scope1, 2: scope2, 3: scope3, 4: scope4 }
+  const scopeIn = () => console.log('scopeFocus scopeIn');
+  const scopeOut = () => console.log('scopeFocus scopeOut');
+  let curId = null;
 
-    init(options);
-    function init(options) {
+  init(options);
 
-        const elems = options.elem.querySelectorAll('[scope-id]');
-        ids = Array.from(elems).reduce((prev, cur) => {
-            const scopeId = cur.getAttribute('scope-id');
-            prev[scopeId] = createScope({
-                id: scopeId,
-                scope: listFocus({ elem: cur }),
-            });
-            return prev;
-        }, {});
+  function init(options) {
 
-        curId = options.initScope;
-        focus(curId);
-    }
+    const elems = options.elem.querySelectorAll('[scope-id]');
+    ids = Array.from(elems).reduce((prev, cur) => {
+      const scopeId = cur.getAttribute('scope-id');
+      prev[scopeId] = createScope({
+        id: scopeId,
+        list: listFocus({elem: cur}),
+      });
+      return prev;
+    }, {});
 
-    // focus算法
-    function newId(curId, direct, oritation) {
-        if(oritation === 'vert') {
-            if(direct === 'left' || direct === 'right') {
-                return null;
-            } else if(direct === 'up'){
-                if(ids[curId - 1]) {
-                    return curId - 1;
-                } else {
-                    return null;
-                }
-            } else if(direct === 'down') {
-                if(ids[curId + 1]) {
-                    return curId + 1;
-                } else {
-                    return null;
-                }
-            }
-        }
-    }
+    curId = options.initScope;
+    focus(curId);
+  }
 
-    function focus(id) {
-        const oldId = curId;
-        if(oldId && oldId !== id) {
-            ids[oldId].scope.scopeOut();
-        }
-        if(id) {
-          ids[id].scope.scopeIn();
-        }
-        curId = id;
-    }
-
-    function update(direct) {
-        const nId = newId(curId, direct, ORITATION);
-        if (nId) {
-            focus(nId);
-        }
-        else {
-            scopeOut();
-        }
-        curId = nId;
-        return curId;
-    }
-
-    function updateFocus(direct) {
-      const itemNewId = ids[curId].scope[direct]();
-      if(itemNewId) {
+  // focus算法
+  function newId(curId, direct, oritation) {
+    if (oritation === 'vert') {
+      if (direct === 'left' || direct === 'right') {
+        return null;
+      } else if (direct === 'up') {
+        if (ids[curId - 1]) {
+          return curId - 1;
+        } else {
           return null;
-      } else {
-          return update(direct);
+        }
+      } else if (direct === 'down') {
+        if (ids[curId + 1]) {
+          return curId + 1;
+        } else {
+          return null;
+        }
       }
     }
+  }
 
-    return {
-      up: () => updateFocus('up'),
-      down: () => updateFocus('down'),
-      left: () => updateFocus('left'),
-      right: () => updateFocus('right'),
-    };
+  function focus(id) {
+    const oldId = curId;
+    if (oldId && oldId !== id) {
+      ids[oldId].list.scopeOut();
+      curId = null;
+    }
+    if (id) {
+      ids[id].list.scopeIn();
+      curId = id;
+    }
+  }
+
+  function scopeMove(direct) {
+    const nId = newId(curId, direct, ORITATION);
+    if (nId) {
+      focus(nId);
+      return true;
+    }
+    else {
+      scopeOut();
+      return false;
+    }
+  }
+
+  function move(direct) {
+    const isSuccess = ids[curId].list.move(direct);
+    if (isSuccess) {
+      return null;
+    } else {
+      return scopeMove(direct);
+    }
+  }
+
+  return {
+    move,
+    scopeIn,
+    scopeOut,
+  };
 }
